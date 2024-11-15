c9f2a3dd669694d646bee14bcb95b4676cf8f73f
Subrata Mondal <162795636+subratamondalnsec@users.noreply.github.com>
2024-11-15 11:12:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function buyNow(product, price) {
+    alert(`You have selected to buy ${product} for ₹${price}`);
+    // Implement the buy functionality here
+}
+
+function buyWithCredits(product, credits) {
+    alert(`You have selected to buy ${product} with ⭐${credits} credits`);
+    // Implement the buy with credits functionality here
+}
+
+function addToFavorites(product) {
+    alert(`${product} has been added to your favorites`);
+    // Implement the add to favorites functionality here
+}
+
+
